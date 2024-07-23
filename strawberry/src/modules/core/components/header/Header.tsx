a985d83c8dce250c2fc03c294e780490d6b55a69
JSK0406 <12180543@inha.edu>
2024-07-23 18:39:42 +0900

    feat: add Header Layout (#5)

---
+++ strawberry/src/modules/core/components/header/Header.tsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import HeaderMenuButtons from "./HeaderMenuButtons";
+import HeaderLogo from "./HeaderLogo";
+import LoginButton from "./LoginButton";
+import CarIntroButton from "./CarIntroButton";
+
+function Header() {
+  return (
+    <StyledHeader>
+      <HeaderWrapper>
+        <HeaderLogo></HeaderLogo>
+        <nav>
+          <HeaderMenuButtons></HeaderMenuButtons>
+        </nav>
+        <ButtonWrapper>
+          <LoginButton></LoginButton>
+          <CarIntroButton></CarIntroButton>
+        </ButtonWrapper>
+      </HeaderWrapper>
+    </StyledHeader>
+  );
+}
+
+export default Header;
+
+const StyledHeader = styled.header`
+  width: 100%;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+`;
+
+const HeaderWrapper = styled.div`
+  width: 1440px;
+  height: 70px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const ButtonWrapper = styled.div`
+  width: 277px;
+  height: 100%;
+  display: flex;
+  margin-left: 70px;
+  justify-content: space-between;
+  align-items: center;
+`;
