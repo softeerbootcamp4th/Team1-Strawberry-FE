accaf5c918a9d9b11f58ebbccd26814f77c33c5f
akrudal <1mmgm.dev@gmail.com>
2024-07-23 15:19:59 +0900

    feat: add theme provider(#4)

--- strawberry/src/main.tsx
+++ strawberry/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import theme from './modules/core/design_system/theme.tsx'
+import { ThemeProvider } from 'styled-components'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ strawberry/src/modules/core/design_system/theme.tsx
@@ -0,0 +1,18 @@
+import Border from "./border";
+import Color from "./color";
+import Shadow from "./shadow";
+import Typography from "./typography";
+import "styled-components";
+
+const theme = {
+    Border,
+    Color,
+    Shadow,
+    Typography
+}
+export default theme;
+
+type ThemeType = typeof theme;
+declare module "styled-components" {
+  export interface DefaultTheme extends ThemeType {}
+}
