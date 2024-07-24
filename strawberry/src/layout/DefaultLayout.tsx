025c0aeabc2113461742a57dfaf4eb15bec5fdf1
JSK0406 <12180543@inha.edu>
2024-07-24 18:33:27 +0900

    feat: implement DefaultLayout (#15)

---
+++ strawberry/src/layout/DefaultLayout.tsx
@@ -0,0 +1,39 @@
+import Header from "./components/header";
+import Footer from "./components/footer";
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+function DefaultLayout() {
+  return (
+    <>
+      <PageWrapper>
+        <HeaderWrapper>
+          <Header />
+        </HeaderWrapper>
+        <Outlet />
+        <FooterWrapper>
+          <Footer />
+        </FooterWrapper>
+      </PageWrapper>
+    </>
+  );
+}
+
+export default DefaultLayout;
+
+const PageWrapper = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  position: relative;
+`;
+const HeaderWrapper = styled.div`
+  width: 100%;
+  position: fixed;
+  top: 0;
+`;
+
+const FooterWrapper = styled.div`
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+`;
