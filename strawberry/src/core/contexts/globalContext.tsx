240af06940ef84a22ace0ae9000170baa49eac2b
JSK0406 <12180543@inha.edu>
2024-07-29 19:31:39 +0900

    feat: add ModalContext, recucer in globalContext (#28)

--- strawberry/src/core/contexts/globalContext.tsx
+++ strawberry/src/core/contexts/globalContext.tsx
@@ -1,62 +1,65 @@
-import { createContext, Dispatch, useReducer } from "react";
+import { createContext, Dispatch, useReducer, ReactNode } from "react";
 
+// User Data Types
 export type UserData = {
   userId: string;
   name: string;
 };
 
+// Global State Types
 export type GlobalState = {
   isLogin: boolean;
   user: UserData | undefined;
+  isModalOpen: boolean;
+  modalType: string;
 };
 
+// Global State Context and Dispatch
 export const GlobalStateContext = createContext<GlobalState | undefined>(
   undefined,
 );
+export const GlobalDispatchContext = createContext<
+  Dispatch<Action> | undefined
+>(undefined);
 
+// Actions
 type Action =
-  | { type: "UPDATE"; data: UserData }
   | { type: "LOGIN"; status: boolean }
-  | { type: "LOGOUT"; status: boolean };
+  | { type: "LOGOUT"; status: boolean }
+  | { type: "UPDATE_USER"; data: UserData }
+  | { type: "CLOSE_MODAL" }
+  | { type: "OPEN_QUIZ_REWARD_MODAL" };
 
-type GlobalDispatch = Dispatch<Action>;
-export const GlobalDispatchContext = createContext<GlobalDispatch | undefined>(
-  undefined,
-);
-
+// Reducer
 function globalReducer(state: GlobalState, action: Action): GlobalState {
-  const newState = state;
-
   switch (action.type) {
     case "LOGIN":
-      newState.isLogin = true;
-      break;
+      return { ...state, isLogin: true };
     case "LOGOUT":
-      newState.isLogin = false;
-      break;
-    case "UPDATE":
-      newState.user = action.data;
-      break;
+      return { ...state, isLogin: false };
+    case "UPDATE_USER":
+      return { ...state, user: action.data };
+    case "CLOSE_MODAL":
+      return { ...state, isModalOpen: false, modalType: "" };
+    case "OPEN_QUIZ_REWARD_MODAL":
+      return { ...state, isModalOpen: true, modalType: "quizReward" };
     default:
       throw new Error("ACTION NOT FOUND");
   }
-
-  return newState;
 }
 
-export function GlobalContextProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const [globalState, dispatch] = useReducer(globalReducer, {
+// Provider
+export function GlobalContextProvider({ children }: { children: ReactNode }) {
+  const [state, dispatch] = useReducer(globalReducer, {
     isLogin: false,
     user: undefined,
+    isModalOpen: false,
+    modalType: "",
   });
 
   return (
     <GlobalDispatchContext.Provider value={dispatch}>
-      <GlobalStateContext.Provider value={globalState}>
+      <GlobalStateContext.Provider value={state}>
         {children}
       </GlobalStateContext.Provider>
     </GlobalDispatchContext.Provider>
