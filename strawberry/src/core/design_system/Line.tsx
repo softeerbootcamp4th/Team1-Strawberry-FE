6cdcdde27de69b21a3369db5d3800c83dd257271
akrudal <1mmgm.dev@gmail.com>
2024-07-29 18:19:24 +0900

    feat: Line Components(#33)

---
+++ strawberry/src/core/design_system/Line.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import theme from "./theme";
+
+interface LineProps {
+  $backgroundcolor?: string;
+  width?: string;
+}
+
+function Line({
+  $backgroundcolor = theme.Color.Common.white,
+  width = "100%",
+}: LineProps) {
+  return <StyledLine $backgroundcolor={$backgroundcolor} width={width} />;
+}
+
+export default Line;
+
+const StyledLine = styled.div<{ $backgroundcolor: string; width: string }>`
+  background-color: ${({ $backgroundcolor }) => $backgroundcolor};
+  height: 1px;
+  width: ${({ width }) => width};
+`;
