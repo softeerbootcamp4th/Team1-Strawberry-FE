82fad4b12a3390129a41f9162da9efa4c2cd6b4e
JiSeong Kim <122510664+JSK0406@users.noreply.github.com>
2024-08-02 18:51:23 +0900

    로그인 페이지를 구현합니다. (#53)
    * feat: add images for login buttons (#34)
    
    * chore: add .env in gitignore (#34)
    
    * feat: add useLogin hook (#34)
    
    * feat: add LoginPage (#34)
    
    * fix: add children element in PublicRoute (#34)
    
    * feat: add LoginPage in router (#34)
    
    * delete dummy file

---
+++ strawberry/src/pages/login/LoginPage.tsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import { Label, Wrapper, theme } from "../../core/design_system";
+
+import naverLoginButton from "/src/assets/images/login/naverLoginButton.svg";
+import hyundaiLoginButton from "/src/assets/images/login/hyundaiLoginButton.svg";
+
+import { useLogin } from "./hooks/useLogin";
+
+function LoginPage() {
+  const login = useLogin();
+
+  return (
+    <>
+      <Wrapper
+        height="calc(100vh - 330px)"
+        display="flex"
+        $flexdirection="column"
+      >
+        <Wrapper
+          height="180px"
+          $backgroundcolor={theme.Color.Ivory.ivory500}
+          display="flex"
+          $justifycontent="center"
+          $alignitems="center"
+          $padding="79px 0 0 0"
+        >
+          <Label $token="Display2Regular">로그인</Label>
+        </Wrapper>
+        <Wrapper
+          display="flex"
+          $flexdirection="column"
+          $alignitems="center"
+          $padding="85px 0 0 0"
+        >
+          <button onClick={() => login({ social: "hyundai" })}>
+            <img src={hyundaiLoginButton} />
+          </button>
+          <StyledButton onClick={() => login({ social: "naver" })}>
+            <img src={naverLoginButton} />
+          </StyledButton>
+        </Wrapper>
+      </Wrapper>
+    </>
+  );
+}
+
+export default LoginPage;
+
+const StyledButton = styled.button`
+  margin-top: 12px;
+`;
--- strawberry/src/router/PublicRoute.tsx
+++ strawberry/src/router/PublicRoute.tsx
@@ -1,9 +1,9 @@
-import { Outlet } from "react-router-dom";
+import { ReactNode } from "react";
 
-function PublicRoute() {
+function PublicRoute({ children }: { children: ReactNode }) {
   // 로그인한 경우 navigate하는 코드 추가
 
-  return <Outlet />;
+  return <>{children}</>;
 }
 
 export default PublicRoute;
