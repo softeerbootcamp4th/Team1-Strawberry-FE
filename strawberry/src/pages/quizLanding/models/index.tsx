69da450a8fa2ea62300db12326a9c0d224945a74
akrudal <1mmgm.dev@gmail.com>
2024-08-06 16:59:10 +0900

    feat: add index.tsx(#63)

---
+++ strawberry/src/pages/quizLanding/models/index.tsx
@@ -0,0 +1,13 @@
+import { Timer } from "./Timer";
+import { PrizeInfo } from "./PrizeInfo";
+import { QuizLanding } from "./QuizLanding";
+import { QuizLandingState } from "./QuizLandingState";
+import { QuizLandingAction } from "./QuizLandingAction";
+
+export type {
+  Timer,
+  PrizeInfo,
+  QuizLanding,
+  QuizLandingState,
+  QuizLandingAction,
+};
