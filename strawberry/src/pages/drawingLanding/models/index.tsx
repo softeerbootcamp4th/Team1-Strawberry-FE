46ed69c85024f14ae1285c9271fa9807adccb3bb
akrudal <1mmgm.dev@gmail.com>
2024-08-07 17:40:40 +0900

    remove and rename files(#65)

--- strawberry/src/pages/drawingLanding/models/index.tsx
+++ strawberry/src/pages/drawingLanding/models/index.tsx
@@ -1,15 +1,13 @@
 import { DrawingLanding } from "./DrawingLanding";
 import { DrawingLandingAction } from "./DrawingLandingAction";
 import { DrawingLandingState } from "./DrawingLandingState";
-import { EventInfo } from "./EventInfo";
 import { EventImageUrl } from "./EventmageUrl";
-import { UserInfo } from "./UserInfo";
+import { EventUserInfo } from "./EventUserInfo";
 
 export type {
   DrawingLanding,
   DrawingLandingAction,
   DrawingLandingState,
-  EventInfo,
   EventImageUrl,
-  UserInfo,
+  EventUserInfo,
 };
