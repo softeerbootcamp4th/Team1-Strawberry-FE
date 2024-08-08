45a0448d937b0658631e90e54a562c2f3cc1c409
JSK0406 <12180543@inha.edu>
2024-08-08 18:36:28 +0900

    feat: add logout logic and change design (#69)

--- strawberry/src/layout/components/header/LogoutButton.tsx
+++ strawberry/src/layout/components/header/LogoutButton.tsx
@@ -1,19 +1,25 @@
 import DefaultButton from "../../../core/design_system/styles/DefaultButton";
 import styled from "styled-components";
-
-// onclick 나중에 추가
+import { useGlobalDispatch } from "../../../core/hooks/useGlobalDispatch";
 
 function LogoutButton() {
-  return <StyledButton>로그아웃</StyledButton>;
+  const dispatch = useGlobalDispatch();
+
+  function handleLogout() {
+    localStorage.removeItem("accessToken");
+    dispatch({ type: "SET_LOGIN", status: false });
+  }
+
+  return <StyledButton onClick={handleLogout}>로그아웃</StyledButton>;
 }
 
 export default LogoutButton;
 
 const StyledButton = styled(DefaultButton)`
-  background-color: ${({ theme }) => theme.Color.Primary.normal};
   ${({ theme }) => theme.Typography.Body1Regular};
   color: ${({ theme }) => theme.Color.TextIcon.default};
   padding: 8px 20px;
   width: 81px;
   height: 40px;
+  white-space: nowrap;
 `;
