4e2b671c7a09db19ff90b4fb6d842e735114e844
JSK0406 <12180543@inha.edu>
2024-08-11 22:20:02 +0900

    feat: add components in LandingPage (#90)

--- strawberry/src/pages/landing/LandingPage.tsx
+++ strawberry/src/pages/landing/LandingPage.tsx
@@ -1,10 +1,18 @@
 import { Wrapper } from "../../core/design_system";
 
+import LandingBanner from "./components/landingBanner/LandingBanner";
+import LandingIntroduce from "./components/landingIntroduce/LandingIntroduce";
+import LandingEvent from "./components/landingEvent/LandingEvent";
+import LandingBottom from "./components/landingBottom/LandingBottom";
+
 function LandingPage() {
   return (
     <>
       <Wrapper display="flex" $flexdirection="column">
-        A
+        <LandingBanner />
+        <LandingIntroduce />
+        <LandingEvent />
+        <LandingBottom />
       </Wrapper>
     </>
   );
