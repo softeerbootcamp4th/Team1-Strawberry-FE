2464aa319f520042a80a409d6ac68169b8dd911f
JSK0406 <12180543@inha.edu>
2024-08-11 17:59:47 +0900

    feat: add LandingPage (#90)

---
+++ strawberry/src/pages/landing/LandingPage.tsx
@@ -0,0 +1,13 @@
+import { Wrapper } from "../../core/design_system";
+
+function LandingPage() {
+  return (
+    <>
+      <Wrapper display="flex" $flexdirection="column">
+        A
+      </Wrapper>
+    </>
+  );
+}
+
+export default LandingPage;
