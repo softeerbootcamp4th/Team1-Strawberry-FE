0ee1271b33f78c73f749ce969ef115dbb885b5a6
JSK0406 <12180543@inha.edu>
2024-08-13 00:11:04 +0900

    fix: change img import (#114)

--- strawberry/src/layout/components/header/HeaderLogo.tsx
+++ strawberry/src/layout/components/header/HeaderLogo.tsx
@@ -1,16 +1,13 @@
 import { Link } from "react-router-dom";
 
+import headerLogo from "/src/assets/images/logos/Hyundai_Motor_Company_logo_1.svg";
+
 // link 추가
 function HeaderLogo() {
   return (
     <div>
       <Link to="/">
-        <img
-          src="/src/assets/images/logos/Hyundai_Motor_Company_logo_1.svg"
-          alt="Logo"
-          width="145px"
-          height="19px"
-        />
+        <img src={headerLogo} alt="Logo" width="145px" height="19px" />
       </Link>
     </div>
   );
