9bd763ea5223579a2ebe2aab770903caf428aa4a
JSK0406 <12180543@inha.edu>
2024-08-12 16:45:00 +0900

    feat: seperate logics (#102)

---
+++ strawberry/src/pages/quizPlay/hooks/logics/useQuizPlayPage.tsx
@@ -0,0 +1,30 @@
+import { useQuizPlayState } from "../useQuizPlayState";
+
+import { useQuizPlayMutation } from "../apis/useQuizPlayMutation";
+
+function useQuizPlayPage() {
+  const { mutate: postQuiz, data: postQuizData } = useQuizPlayMutation();
+  const {
+    description,
+    question,
+    subDescription,
+    placeholder,
+    hint,
+    answer,
+    subEventId,
+  } = useQuizPlayState();
+
+  return {
+    description,
+    question,
+    subDescription,
+    placeholder,
+    hint,
+    answer,
+    subEventId,
+    postQuiz,
+    postQuizData,
+  };
+}
+
+export default useQuizPlayPage;
