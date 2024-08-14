1ff41ffa2d74d96327837073baaa461ed07c38fa
g_mi <62610032+akrudal@users.noreply.github.com>
2024-08-14 12:21:12 +0900

    공통 Carousel을 만듭니다. (#131)

--- strawberry/src/pages/drawingPlay/components/guide/GuideCarouselWrapper.tsx
+++ strawberry/src/pages/drawingPlay/components/guide/GuideCarouselWrapper.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 import { Wrapper } from "../../../../core/design_system";
+import Carousel from "../../../common/components/carousel/Carousel";
 
 import { guides } from "../../models/Guide";
 
@@ -8,12 +9,6 @@ import { useDrawingPlayState } from "../../hooks/useDrawingPlayState";
 import { useDrawingPlayDispatch } from "../../hooks/useDrawingPlayDispatch";
 
 import DrawingPractice from "./DrawingPractice";
-import GuideCarousel from "./GuideCarousel";
-
-interface IndicatorProps {
-  active: boolean;
-  onClick: () => void;
-}
 
 function GuideCarouselWrapper() {
   const { guideIndex } = useDrawingPlayState();
@@ -23,13 +18,15 @@ function GuideCarouselWrapper() {
     dispatch({ type: "SET_GUIDE_INDEX", payload: index });
   };
 
+  const visibleGuides = guides.slice(0, guides.length - 1);
+
   return (
     <Wrapper height="440px" $position="relative">
-      <GuideCarousel curr={guideIndex} onSlideChange={handleSlideChange}>
-        {guides.slice(0, guides.length - 1).map((guide, index) => (
-          <Wrapper key={index} width="100%" $margin="0 auto">
-            <img width="100%" src={guide.image} />
-          </Wrapper>
+      <Carousel curr={guideIndex} onSlideChange={handleSlideChange}>
+        {visibleGuides.map((guide, index) => (
+          <GuideSlide key={index}>
+            <img width="100%" src={guide.image} alt={`Guide ${index + 1}`} />
+          </GuideSlide>
         ))}
         <Wrapper
           $position="relative"
@@ -41,16 +38,7 @@ function GuideCarouselWrapper() {
             <DrawingPractice timeLimit={3} />
           </DrawingCanvasWrapper>
         </Wrapper>
-      </GuideCarousel>
-      <Indicators>
-        {guides.slice(0, guides.length).map((_, i) => (
-          <Indicator
-            key={i}
-            active={guideIndex === i}
-            onClick={() => handleSlideChange(i)}
-          />
-        ))}
-      </Indicators>
+      </Carousel>
     </Wrapper>
   );
 }
@@ -64,26 +52,7 @@ const DrawingCanvasWrapper = styled.div`
   transform: translateX(-50%);
 `;
 
-const Indicators = styled.div`
-  position: absolute;
-  bottom: 20px;
-  left: 50%;
-  transform: translateX(-50%);
-  display: flex;
-  gap: 8px;
-  z-index: 10;
-`;
-
-const Indicator = styled.div<IndicatorProps>`
-  width: ${({ active }) => (active ? "14px" : "12px")};
-  height: ${({ active }) => (active ? "14px" : "12px")};
-  background-color: ${({ theme }) => theme.Color.Neutral.neutral90};
-  border-radius: 50%;
-  opacity: ${({ active }) => (active ? 1 : 0.5)};
-  transition: all 0.3s;
-  cursor: pointer;
-
-  &:hover {
-    opacity: 1;
-  }
+const GuideSlide = styled(Wrapper)`
+  width: 100%;
+  margin: 0 auto;
 `;
