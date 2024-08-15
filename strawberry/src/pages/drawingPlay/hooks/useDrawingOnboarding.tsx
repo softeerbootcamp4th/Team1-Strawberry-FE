ac1cb7b74898081617682d78ec73ced7f1a1ecd3
JSK0406 <12180543@inha.edu>
2024-08-15 19:36:50 +0900

    feat: add server state in DrawingOnboarding and fix modal logic (#121)

--- strawberry/src/pages/drawingPlay/hooks/useDrawingOnboarding.tsx
+++ strawberry/src/pages/drawingPlay/hooks/useDrawingOnboarding.tsx
@@ -3,22 +3,18 @@ import { useEffect } from "react";
 import { useDrawingPlayDispatch } from "./useDrawingPlayDispatch";
 import { useDrawingPlayState } from "./useDrawingPlayState";
 
-import santafeBG from "/src/assets/images/background/santafeBG.png";
-import interiorBG from "/src/assets/images/background/interiorBG.png";
-import exteriorBG from "/src/assets/images/background/exteriorBG.png";
-
 export function useDrawingOnboarding() {
-  const { stage, totalStage, isGuideOpen } = useDrawingPlayState();
+  const { stage, totalStage, isGuideOpen, drawingInfo } = useDrawingPlayState();
   const dispatch = useDrawingPlayDispatch();
 
   useEffect(() => {
     if (!localStorage.getItem("hideGuideModalUntil") && isGuideOpen !== false) {
       dispatch({ type: "SET_GUIDE_OPEN", payload: true });
     }
-  }, []);
+  }, [isGuideOpen, dispatch]);
 
   useEffect(() => {
-    if (isGuideOpen === false) {
+    if (localStorage.getItem("hideGuideModalUntil") || isGuideOpen === false) {
       const id = setTimeout(() => {
         dispatch({ type: "SET_GAME" });
       }, 3000);
@@ -30,16 +26,8 @@ export function useDrawingOnboarding() {
   return {
     stage,
     totalStage,
-    imgPath: imgPaths[stage],
-    title: titles[stage],
+    imgPath: drawingInfo?.gameInfos?.[stage - 1].imgUrl ?? "",
+    title: drawingInfo?.gameInfos?.[stage - 1].onBoardingMsg ?? "",
     isGuideOpen,
   };
 }
-
-const imgPaths = ["", santafeBG, interiorBG, exteriorBG];
-const titles = [
-  "",
-  "3초 후 디 올 뉴 싼타페를 그려주세요!",
-  "3초 후 디 올 뉴 싼타페의 space를 그려주세요!",
-  "3초 후 디 올 뉴 싼타페의 space를 그려주세요!",
-];
