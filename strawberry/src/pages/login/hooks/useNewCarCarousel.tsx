42955283f6cedc9463bbdf2e34b0b0d3c97e1688
JiSeong Kim <122510664+JSK0406@users.noreply.github.com>
2024-08-15 20:38:28 +0900

    신차 소개 페이지의 캐러셀을 구현합니다. (#137)
    * fea: add images (#136)
    
    * feat: add useNewCarCarousel hook (#136)
    
    * feat: implement NewCarCarousel (#136)

---
+++ strawberry/src/pages/login/hooks/useNewCarCarousel.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+function useNewCarCarousel(length: number) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [translationStep, setTranslationStep] = useState(0);
+
+  const calculateTranslationStep = () => {
+    const browserWidth = window.innerWidth;
+    const shownWidth = browserWidth - (browserWidth - 1440) / 2 - 535;
+    const fullImgWidth = 665 * length + 48 * (length - 1);
+    return (fullImgWidth - shownWidth) / (length - 1);
+  };
+
+  useEffect(() => {
+    setTranslationStep(calculateTranslationStep());
+  }, [length]);
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? 0 : prevIndex - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === length - 1 ? length - 1 : prevIndex + 1,
+    );
+  };
+
+  return { currentIndex, translationStep, handlePrev, handleNext };
+}
+
+export default useNewCarCarousel;
