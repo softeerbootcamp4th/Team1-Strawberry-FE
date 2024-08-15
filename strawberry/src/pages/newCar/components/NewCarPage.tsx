68a1ff7c83ed0cf090e714d30338e30c72773eb1
Kyoungmi <1mmgm.dev@gmail.com>
2024-08-15 21:51:20 +0900

    feat: add new car page(#108)

--- strawberry/src/pages/newCar/components/NewCarPage.tsx
+++ strawberry/src/pages/newCar/components/NewCarPage.tsx
@@ -1,7 +1,16 @@
+import { Wrapper } from "../../../core/design_system";
 import NewCarBanner from "./banner/NewCarBanner";
+import NewCarDesign from "./design/NewCarDesign";
+import NewCarParts from "./part/NewCarParts";
 
 function NewCarPage() {
-  return <NewCarBanner />;
+  return (
+    <Wrapper height="fit-content">
+      <NewCarBanner />
+      <NewCarParts />
+      <NewCarDesign />
+    </Wrapper>
+  );
 }
 
 export default NewCarPage;
