962b5f5db9fa8a7409ce758af5fd5daea0655d5f
akrudal <1mmgm.dev@gmail.com>
2024-08-16 20:49:51 +0900

    refactor :new car carousel(#162)

--- strawberry/src/pages/newCar/components/driving/DrivingCarousel.tsx
+++ strawberry/src/pages/newCar/components/driving/DrivingCarousel.tsx
@@ -1,20 +1,15 @@
-import { NewCarCarouselData } from "../../models/NewCarCarouselData";
+import { Wrapper } from "../../../../core/design_system";
+
+import { drivingData } from "../../models/NewCarCarouselItem";
+
 import NewCarCarousel from "../NewCarCarousel";
 
 function DrivingCarousel() {
-  return <NewCarCarousel data={drivingData}></NewCarCarousel>;
+  return (
+    <Wrapper $padding="80px 0">
+      <NewCarCarousel item={drivingData} />
+    </Wrapper>
+  );
 }
 
 export default DrivingCarousel;
-
-const drivingData: NewCarCarouselData = {
-  title: "Hybrid",
-  description:
-    "e-Motion Drive의 하이브리드 특화 기술로\n진보된 주행 성능을 제공합니다",
-  imgs: [
-    "/src/assets/images/newCar/hybrid_1.svg",
-    "/src/assets/images/newCar/hybrid_2.svg",
-    "/src/assets/images/newCar/hybrid_3.svg",
-    "/src/assets/images/newCar/hybrid_4.svg",
-  ],
-};
