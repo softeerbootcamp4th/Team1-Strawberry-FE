00bf9a3a9a92dfdd63f055e7505d6a8a7c42db23
akrudal <1mmgm.dev@gmail.com>
2024-08-16 20:49:18 +0900

    refactor: new car space(#162)

--- strawberry/src/pages/newCar/components/space/SpaceBanner.tsx
+++ strawberry/src/pages/newCar/components/space/SpaceBanner.tsx
@@ -1,14 +1,14 @@
-import { ImageEnum } from "../../../../core/design_system";
+import { ImageEnum, Wrapper } from "../../../../core/design_system";
 
 function SpaceBanner() {
   return (
-    <>
+    <Wrapper $padding="100px 0">
       <img
         src={ImageEnum.IMAGES.NEWCAR.SPACE_BANNER}
         alt="spaceBanner"
         width="100%"
       />
-    </>
+    </Wrapper>
   );
 }
 
--- strawberry/src/pages/newCar/components/space/SpaceSeats.tsx
+++ strawberry/src/pages/newCar/components/space/SpaceSeats.tsx
@@ -5,6 +5,7 @@ import { Wrapper, ImageEnum } from "../../../../core/design_system";
 type seatsType = 5 | 6 | 7;
 
 import styled from "styled-components";
+import FoundationButton from "../FoundationButton";
 
 function SpaceSeats() {
   const [seats, setSeats] = useState<seatsType>(5);
@@ -24,9 +25,21 @@ function SpaceSeats() {
           $justifycontent="flex-end"
           $margin="0 0 24px 0"
         >
-          <button onClick={() => setSeats(5)}>5인승</button>
-          <button onClick={() => setSeats(6)}>6인승</button>
-          <button onClick={() => setSeats(7)}>7인승</button>
+          <FoundationButton
+            variant="SOLID"
+            // onClick={() => setSeats(5)}
+            title="5인승"
+          />
+          <FoundationButton
+            variant="OUTLINE"
+            // onClick={() => setSeats(6)}
+            title="6인승"
+          />
+          <FoundationButton
+            variant="OUTLINE"
+            // onClick={() => setSeats(7)}
+            title="7인승"
+          />
         </Wrapper>
         <Wrapper>
           <img src={imgs[seats]} alt="img" width="100%" />
@@ -39,9 +52,10 @@ function SpaceSeats() {
 export default SpaceSeats;
 
 const NewCarSeatsWrapper = styled.div`
-  width: 100%;
+  margin: 0 auto;
+  width: 1440px;
   display: flex;
   flex-direction: column;
-  padding: 64px 240px;
+  padding: 64px 0;
   box-sizing: border-box;
 `;
