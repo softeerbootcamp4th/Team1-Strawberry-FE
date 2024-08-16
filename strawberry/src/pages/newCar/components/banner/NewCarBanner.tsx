07085a5fd4a04a07dc1af50c193bf45f36eaaff4
akrudal <1mmgm.dev@gmail.com>
2024-08-16 20:50:06 +0900

    refactor: new car pages(#162)

--- strawberry/src/pages/newCar/components/banner/NewCarBanner.tsx
+++ strawberry/src/pages/newCar/components/banner/NewCarBanner.tsx
@@ -1,8 +1,6 @@
 import styled from "styled-components";
 
-import Background1 from "./Background1.svg";
-import Background2 from "./Background2.svg";
-import BannerSantfae from "./BannerSantafe.svg";
+import { ImageEnum } from "../../../../core/design_system";
 
 import ScrollButton from "../../../common/components/buttons/scrollButton/ScrollButton";
 
@@ -15,13 +13,8 @@ function NewCarBanner() {
     <RefWrapper ref={heightRef}>
       <EventNameLabel>일상과 낭만 사이</EventNameLabel>
       <CarNameLabel>The all-new SANTA FE</CarNameLabel>
-      <Background1Img src={Background1} />
-      <Background2Img src={Background2} />
-      <CarImg src={BannerSantfae} />
-      <DimmerWrapper>
-        <UpDimmer />
-        <BottomDimmer />
-      </DimmerWrapper>
+      <BackgroundImg src={ImageEnum.IMAGES.NEWCAR.NEWCAR_BANNER_BACKGROUND} />
+      <CarImg src={ImageEnum.IMAGES.NEWCAR.NEWCAR_BANNER_CAR} />
       <ScrollButtonWrapper>
         <ScrollButton scrollHeight={scrollHeight} />
       </ScrollButtonWrapper>
@@ -79,69 +72,13 @@ const CarNameLabel = styled.p`
   mix-blend-mode: overlay;
 `;
 
-const Background1Img = styled.img`
+const BackgroundImg = styled.img`
   position: absolute;
-  top: 0;
-  left: 300px;
   z-index: 3;
 
-  /* width: 40%; */
+  width: 100%;
   height: 100%;
   vertical-align: top;
-`;
-
-const Background2Img = styled.img`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  z-index: 2;
-
-  width: 100%;
-  vertical-align: bottom;
-
-  fill:
-    linear-gradient(0deg, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0.3) 100%),
-    url(<path-to-image>) lightgray -0.198px -120.729px / 100.02% 113.957%
-      no-repeat;
-
-  opacity: 0.8;
-  mix-blend-mode: soft-light;
-`;
-
-const DimmerWrapper = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  z-index: 5;
-
-  width: 100%;
-  height: 42%;
-`;
-
-const UpDimmer = styled.div`
-  width: 100%;
-  height: 18%;
-
-  opacity: 0.8;
-  background: linear-gradient(
-      90deg,
-      rgba(215, 215, 211, 0.8) 0%,
-      rgba(162, 162, 162, 0.8) 100%
-    ),
-    rgba(215, 215, 211, 0.6);
-  filter: blur(25px);
-`;
-
-const BottomDimmer = styled.div`
-  width: 100%;
-  height: 82%;
-  background: linear-gradient(
-    180deg,
-    rgba(64, 64, 64, 0) 0%,
-    rgba(33, 33, 33, 0.22) 33%,
-    rgba(13, 13, 13, 0.59) 61.5%,
-    rgba(4, 4, 4, 0.7) 100%
-  );
 `;
 
 const CarImg = styled.img`
