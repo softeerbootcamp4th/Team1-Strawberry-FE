04a3c5c9d64146b7b7e470d3171b19e78d816b32
JiSeong Kim <122510664+JSK0406@users.noreply.github.com>
2024-08-16 10:18:37 +0900

    신차소개 페이지의 SpaceBanner를 구현합니다. (#148)
    * feat: add image (#147)
    
    * feat: implement SpaceBanner Component (#147)

---
+++ strawberry/src/pages/newCar/components/spaceBanner/SpaceBanner.tsx
@@ -0,0 +1,11 @@
+import spaceBanner from "/src/assets/images/newCar/space_banner.svg";
+
+function SpaceBanner() {
+  return (
+    <>
+      <img src={spaceBanner} alt="spaceBanner" width="100%" />
+    </>
+  );
+}
+
+export default SpaceBanner;
