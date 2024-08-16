4fc9df9dd5805f7d63de5ee849c7cbc6e5ce21df
JiSeong Kim <122510664+JSK0406@users.noreply.github.com>
2024-08-16 10:16:48 +0900

    신차 소개 페이지의 Driving 배너를 구현합니다. (#144)
    * feat: add image (#139)
    
    * feat: implement DrvingBanner (#139)

---
+++ strawberry/src/pages/newCar/components/drivingBanner/DrivingBanner.tsx
@@ -0,0 +1,13 @@
+import { Wrapper } from "../../../../core/design_system";
+
+import drivingBanner from "/src/assets/images/newCar/driving_banner.svg";
+
+function DrivingBanner() {
+  return (
+    <>
+      <img src={drivingBanner} alt="drivingBanner" width="100%" />
+    </>
+  );
+}
+
+export default DrivingBanner;
