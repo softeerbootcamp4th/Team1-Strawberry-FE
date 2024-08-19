3ab96694e950ab27b94443cd551ce2423d6b87e4
JiSeong Kim <122510664+JSK0406@users.noreply.github.com>
2024-08-19 10:25:01 +0900

    퀴즈 플레이에서 입력 box를 hightlight하는 기능을 추가합니다. (#191)

--- strawberry/src/pages/quizPlay/components/quizInput/CardInput.tsx
+++ strawberry/src/pages/quizPlay/components/quizInput/CardInput.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import styled from "styled-components";
 
 import { useGlobalState } from "../../../../core/hooks/useGlobalState";
@@ -15,6 +15,7 @@ const CardInput = (props: CardInputProps) => {
   const { length, placeholder, handleSubmit } = props;
 
   const inputRef = useRef<HTMLInputElement>(null);
+  const [highlightedIndex, setHighlightedIndex] = useState<number | null>(null);
 
   const { isModalOpen } = useGlobalState();
   const { answer: content } = useQuizPlayState();
@@ -24,6 +25,8 @@ const CardInput = (props: CardInputProps) => {
     if (inputRef.current) {
       inputRef.current.focus();
     }
+
+    setHighlightedIndex(0);
   }, []);
 
   useEffect(() => {
@@ -34,14 +37,25 @@ const CardInput = (props: CardInputProps) => {
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
+
     if (value.length <= length) {
       dispatch({ type: "SET_ANSWER", payload: value });
+
+      if (value.length > content.length) {
+        setHighlightedIndex(value.length - 1);
+      }
     }
   };
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "ArrowLeft" || event.key === "ArrowRight") {
       event.preventDefault();
+    }
+
+    if (event.key === "Backspace") {
+      if (content.length > 0) {
+        setHighlightedIndex(content.length - 1);
+      }
     }
 
     if (event.key === "Enter" && content.length === length) {
@@ -54,12 +68,13 @@ const CardInput = (props: CardInputProps) => {
     if (inputRef.current) {
       inputRef.current.focus();
       inputRef.current.setSelectionRange(content.length, content.length);
+      setHighlightedIndex(content.length - 1);
     }
   };
 
   return (
     <Container onClick={handleClick} length={length}>
-      <HiddenInput
+      <StyledInput
         ref={inputRef}
         type="text"
         value={content}
@@ -69,17 +84,7 @@ const CardInput = (props: CardInputProps) => {
       />
       <VisibleContent>
         {Array.from({ length }).map((_, index) => (
-          <CharacterBox
-            key={index}
-            onClick={(e) => {
-              e.stopPropagation();
-              inputRef.current?.focus();
-              inputRef.current?.setSelectionRange(
-                content.length,
-                content.length,
-              );
-            }}
-          >
+          <CharacterBox key={index} isHighlighted={index === highlightedIndex}>
             <Character isFilled={Boolean(content[index])}>
               {content[index] || placeholder[index]}
             </Character>
@@ -102,7 +107,7 @@ const Container = styled.div<{ length: number }>`
   cursor: text;
 `;
 
-const HiddenInput = styled.input`
+const StyledInput = styled.input`
   width: 100%;
   height: 100%;
   padding: 0;
@@ -128,7 +133,7 @@ const VisibleContent = styled.div`
   z-index: 1;
 `;
 
-const CharacterBox = styled.div`
+const CharacterBox = styled.div<{ isHighlighted: boolean }>`
   width: 130px;
   height: 130px;
   display: flex;
@@ -140,6 +145,8 @@ const CharacterBox = styled.div`
   box-sizing: border-box;
   position: relative;
   cursor: pointer;
+
+  ${({ isHighlighted }) => isHighlighted && `border: 3px solid #FF8126;`}
 
   &:last-child {
     margin-right: 0;
