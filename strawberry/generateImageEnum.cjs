882d9d642fd1aab20007fe2bd96ed0947e193cc5
JiSeong Kim <122510664+JSK0406@users.noreply.github.com>
2024-08-19 14:51:15 +0900

    드로잉 결과 페이지의 gif를 추가합니다. (#203)
    * feat: add gif in generateImageEnum (#202)
    
    * feat: replace url with local path (#202)

--- strawberry/generateImageEnum.cjs
+++ strawberry/generateImageEnum.cjs
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 // 이미지 파일이 있는 기본 디렉토리
-const baseDir = path.join(__dirname, 'public/assets/');
+const baseDir = path.join(__dirname, "public/assets/");
 
 // 디렉토리를 재귀적으로 탐색하여 이미지 파일 경로를 수집
 const getImageFiles = (dir) => {
@@ -18,7 +18,7 @@ const getImageFiles = (dir) => {
         name: file,
         files: getImageFiles(filePath),
       });
-    } else if (/\.(jpg|jpeg|png|webp|svg)$/i.test(file)) {
+    } else if (/\.(jpg|jpeg|png|webp|svg|gif)$/i.test(file)) {
       results.push(filePath);
     }
   });
@@ -27,20 +27,23 @@ const getImageFiles = (dir) => {
 };
 
 // 이미지를 enum으로 변환
-const generateEnum = (files, parentKey = '') => {
-  let enumString = '';
+const generateEnum = (files, parentKey = "") => {
+  let enumString = "";
 
   files.forEach((file) => {
-    if (typeof file === 'object') {
+    if (typeof file === "object") {
       // 하위 디렉토리의 enum을 생성
-      const key = file.name.toUpperCase().replace(/[^A-Z0-9]/g, '_');
+      const key = file.name.toUpperCase().replace(/[^A-Z0-9]/g, "_");
       enumString += `  ${key}: {\n`;
       enumString += generateEnum(file.files, key);
       enumString += `  },\n`;
     } else {
       // 이미지 파일의 enum을 생성
-      const relativePath = path.relative(baseDir, file).replace(/\\/g, '/');
-      const fileName = path.basename(file, path.extname(file)).toUpperCase().replace(/[^A-Z0-9]/g, '_');
+      const relativePath = path.relative(baseDir, file).replace(/\\/g, "/");
+      const fileName = path
+        .basename(file, path.extname(file))
+        .toUpperCase()
+        .replace(/[^A-Z0-9]/g, "_");
       enumString += `    ${fileName}: "/assets/${relativePath}",\n`;
     }
   });
@@ -51,11 +54,14 @@ const generateEnum = (files, parentKey = '') => {
 // 메인 함수
 const main = () => {
   const imageFiles = getImageFiles(baseDir);
-  let enumString = 'export const ImageEnum = {\n';
+  let enumString = "export const ImageEnum = {\n";
   enumString += generateEnum(imageFiles);
-  enumString += '}\n';
-  fs.writeFileSync(path.join(__dirname, 'src/core/design_system/ImageEnum.ts'), enumString);
-  console.log('Enum file generated!');
+  enumString += "}\n";
+  fs.writeFileSync(
+    path.join(__dirname, "src/core/design_system/ImageEnum.ts"),
+    enumString,
+  );
+  console.log("Enum file generated!");
 };
 
 main();
