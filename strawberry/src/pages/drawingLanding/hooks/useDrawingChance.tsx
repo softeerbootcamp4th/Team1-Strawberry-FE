b35c6d389c88d82428a7ce0b06c1a5f92f97c712
JSK0406 <12180543@inha.edu>
2024-08-19 20:44:15 +0900

    feat: delete lastVisitedAt and refactoring (#175)

--- strawberry/src/pages/drawingLanding/hooks/useDrawingChance.tsx
+++ strawberry/src/pages/drawingLanding/hooks/useDrawingChance.tsx
@@ -1,57 +1,35 @@
 import { useMemo } from "react";
-import { EventUserInfo } from "../models";
+import { EventUserInfo } from "../../../data/entities/EventUserInfo";
 
-function formatLastPlayTime(lastPlayTime: string): string {
-  const lastPlayDate = new Date(lastPlayTime);
-
-  const hours = lastPlayDate.getHours().toString().padStart(2, "0");
-  const minutes = lastPlayDate.getMinutes().toString().padStart(2, "0");
-  return `마지막 게임 참여 시간 : ${hours}시 ${minutes}분`;
-}
-
-function formatTimeUntilNextChance(lastPlayTime: string): string {
-  const lastPlayDate = new Date(lastPlayTime);
-  const nextChanceDate = new Date(lastPlayDate.getTime() + 8 * 60 * 60 * 1000);
-
-  const now = new Date();
-  const timeDifference = nextChanceDate.getTime() - now.getTime();
-
-  if (timeDifference <= 0) {
-    return "1회 도전할 수 있어요!";
-  }
-
-  const hoursRemaining = Math.floor(timeDifference / (1000 * 60 * 60));
-  const minutesRemaining = Math.floor(
-    (timeDifference % (1000 * 60 * 60)) / (1000 * 60),
-  );
-
-  return `${hoursRemaining}시간 ${minutesRemaining}분 후에 도전할 수 있어요!`;
-}
+import { getChanceFromLastTime } from "../services/getChanceFromLastTime";
+import { formatTimeUntilNextChance } from "../services/formatTimeUntilNextChance";
 
 export function useDrawingChance(eventUserData: EventUserInfo | undefined) {
   return useMemo(() => {
     if (!eventUserData) {
-      return;
+      return "";
     }
 
-    const chance: number = eventUserData?.chance ?? 0;
+    let chance: number = eventUserData.chance ?? 0;
     const bonusChance: number =
-      (eventUserData?.expectationBonusChance ?? 0) +
-      (eventUserData?.shareBonusChance ?? 0);
+      (eventUserData.expectationBonusChance ?? 0) +
+      (eventUserData.shareBonusChance ?? 0);
 
-    const lastPlayTime: string = eventUserData?.lastVisitedAt ?? "";
-    const formattedLastPlayTime = formatLastPlayTime(lastPlayTime);
+    const lastPlayTime: string = eventUserData.lastChargeAt ?? "";
+
+    const chanceFromLastTime = getChanceFromLastTime(lastPlayTime);
+    chance = Math.min(2, chance + chanceFromLastTime);
 
     let text = "";
 
     if (chance > 0) {
       if (bonusChance > 0) {
-        text = `${formattedLastPlayTime}\n${chance}회 + 보너스 ${bonusChance}회, 총 ${chance + bonusChance}회 도전할 수 있어요!`;
+        text = `${chance}회 + 보너스 ${bonusChance}회, 총 ${chance + bonusChance}회 도전할 수 있어요!`;
       } else {
-        text = `${formattedLastPlayTime}\n${chance}회 도전할 수 있어요!`;
+        text = `${chance}회 도전할 수 있어요!`;
       }
     } else {
-      text = `${formattedLastPlayTime}\n${formatTimeUntilNextChance(lastPlayTime)}`;
+      text = `${formatTimeUntilNextChance(lastPlayTime)}`;
     }
 
     return text;
