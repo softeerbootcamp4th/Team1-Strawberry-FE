361f7150ff0a6aa8458dfb9e1bfab8c564a03380
g_mi <62610032+akrudal@users.noreply.github.com>
2024-08-19 14:50:56 +0900

    라디오버튼을 만듭니다. (#205)
    * feat: add radio buttons(#197)
    
    * feat: remove dispatch props(#197)
    
    * feat: remove generic type(#197)

--- strawberry/src/pages/newCar/components/design/ExteriorCalligraphy.tsx
+++ strawberry/src/pages/newCar/components/design/ExteriorCalligraphy.tsx
@@ -1,82 +1,42 @@
 import { Wrapper, Label } from "../../../../core/design_system";
-import FoundationButton from "../FoundationButton";
+import { useNewCarDispatch } from "../../hooks/useNewCarDispatch";
 
-type CalligraphyType = "NORMAL" | "BLACKINK";
-
-interface Calligraphy {
-  type: CalligraphyType;
-  name: string;
-  color: string;
-}
-
-const calligraphyDatas: Calligraphy[] = [
-  {
-    type: "NORMAL",
-    name: "얼씨 브래스 메탈릭 매트",
-    color: "brown",
-  },
-  {
-    type: "NORMAL",
-    name: "크리미 화이트 펄",
-    color: "whitePearl",
-  },
-  {
-    type: "NORMAL",
-    name: "사이버 세이지 펄",
-    color: "cyber",
-  },
-  {
-    type: "NORMAL",
-    name: "페블 블루 펄",
-    color: "blue",
-  },
-  {
-    type: "NORMAL",
-    name: "오카도 그린 펄",
-    color: "green",
-  },
-  {
-    type: "NORMAL",
-    name: "테라코타 오렌지",
-    color: "orange",
-  },
-  {
-    type: "NORMAL",
-    name: "마그네틱 그레이 메탈릭",
-    color: "gray",
-  },
-  {
-    type: "NORMAL",
-    name: "어비스 블랙 펄",
-    color: "black",
-  },
-  {
-    type: "BLACKINK",
-    name: "크리미 화이트 펄",
-    color: "whitePearl",
-  },
-  {
-    type: "BLACKINK",
-    name: "크리미 화이트 매트",
-    color: "whiteMatte",
-  },
-  {
-    type: "BLACKINK",
-    name: "어비스 블랙 펄",
-    color: "black",
-  },
-];
+import { useNewCarState } from "../../hooks/useNewCarState";
+import { ExteriorCalligraphyType } from "../../models";
+import FoundationRadioButtons from "../FoundationRadioButton";
 
 function ExteriorCalligraphy() {
+  const buttons = [
+    { title: "캘리그래피", value: "NORMAL" as ExteriorCalligraphyType },
+    {
+      title: "캘리그래피(블랙잉크)",
+      value: "BLACKINK" as ExteriorCalligraphyType,
+    },
+  ];
+
+  const state = useNewCarState();
+  const dispatch = useNewCarDispatch();
+
+  const actionCreator = (value: ExteriorCalligraphyType) => {
+    dispatch({
+      type: "SET_EXTERIOR_CALLIGRAPHY_TYPE",
+      newType: value,
+    });
+  };
+
+  const selector = state.exteriorCalligraphyType;
+
   return (
     <Wrapper>
       <Wrapper>
-        <FoundationButton variant="SOLID" title="캘리그래피" />
-        <FoundationButton variant="OUTLINE" title="캘리그래피(블랙잉크)" />
+        <FoundationRadioButtons<ExteriorCalligraphyType>
+          buttons={buttons}
+          actionCreator={actionCreator}
+          selector={selector}
+        />
       </Wrapper>
       <img />
-      <Label $token="Title2Medium"></Label>
-      {}
+      <Label $token="Title2Medium">{state.exteriorCalligraphyType}</Label>
     </Wrapper>
   );
 }
