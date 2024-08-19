0f5ee1130340cc49c944fbf1740ee3d917f1d319
akrudal <1mmgm.dev@gmail.com>
2024-08-19 19:01:02 +0900

    feat: add custom error(#208)

--- strawberry/src/data/config/customError.tsx
+++ strawberry/src/data/config/customError.tsx
@@ -1,16 +1,42 @@
-export const ErrorMessages = {
-  NETWORK_ERROR:
-    "Network error occurred. Please check your internet connection.",
-  TOKEN_ERROR: "Authentication error. Please log in again.",
-  UNKNOWN_ERROR: "An unknown error occurred. Please try again later.",
-  PARSE_ERROR: "Failed to parse the server response. Please contact support.",
+type ErrorType =
+  | "BAD_REQUEST"
+  | "UNAUTHORIZED"
+  | "FORBIDDEN"
+  | "NOT_FOUND"
+  | "INTERNAL_SERVER_ERROR";
+
+const ErrorStatus: { [key: number]: ErrorType } = {
+  400: "BAD_REQUEST",
+  401: "UNAUTHORIZED",
+  403: "FORBIDDEN",
+  404: "NOT_FOUND",
+  500: "INTERNAL_SERVER_ERROR",
+};
+
+export const ErrorMessages: { [K in ErrorType]: string } = {
+  INTERNAL_SERVER_ERROR: "An unknown error occurred. Please try again later.",
+  BAD_REQUEST: "Bad request. Please check the input and try again.",
+  UNAUTHORIZED: "You are not authorized. Please log in.",
+  FORBIDDEN: "Access forbidden. You don't have permission.",
+  NOT_FOUND: "The requested resource was not found.",
 } as const;
 
-export type ErrorType = keyof typeof ErrorMessages;
+const getMessageWithStatus = (status: number): string => {
+  const errorType = ErrorStatus[status];
+  return ErrorMessages[errorType] || "An unexpected error occurred.";
+};
 
 export class CustomError extends Error {
-  constructor(public type: ErrorType) {
-    super(ErrorMessages[type]);
-    this.name = "BaseError";
+  public status?: number;
+  public type?: ErrorType;
+
+  constructor(status: number, isCustom: boolean, message?: string) {
+    const finalMessage = isCustom ? message : getMessageWithStatus(status);
+    super(finalMessage);
+
+    this.name = "CustomError";
+    this.status = status;
+
+    this.type = ErrorStatus[status];
   }
 }
