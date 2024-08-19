1fdab5f514a5e7f17af1817be4e309c5feb24350
JSK0406 <12180543@inha.edu>
2024-08-20 01:58:59 +0900

    feat: add opening modal depending on isLoading with mutation (#218)

--- strawberry/src/pages/quizPlay/hooks/logics/useQuizPlayPage.tsx
+++ strawberry/src/pages/quizPlay/hooks/logics/useQuizPlayPage.tsx
@@ -1,9 +1,33 @@
-import { useQuizPlayState } from "../useQuizPlayState";
+import { useEffect } from "react";
+
+import { useGlobalDispatch } from "../../../../core/hooks/useGlobalDispatch";
 
 import { useQuizPlayMutation } from "../../../../data/queries/quiz/useQuizPlayMutation";
 
+import { useQuizPlayState } from "../useQuizPlayState";
+
 function useQuizPlayPage() {
-  const { mutate: postQuiz } = useQuizPlayMutation();
+  const { mutate: postQuiz, isLoading } = useQuizPlayMutation();
+  const globalDispatch = useGlobalDispatch();
+
+  useEffect(() => {
+    let timeoutId: number | undefined;
+
+    if (isLoading) {
+      timeoutId = setTimeout(() => {
+        globalDispatch?.({
+          type: "OPEN_MODAL",
+          modalCategory: "PROGRESS",
+          modalProps: {},
+        });
+      }, 200); // 0.2초 안에 결과가 나오면 로딩창 안 보여줌
+    } else {
+      clearTimeout(timeoutId);
+    }
+
+    return () => clearTimeout(timeoutId);
+  }, [isLoading, globalDispatch]);
+
   const {
     description,
     question,
