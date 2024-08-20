e3ebd58e16e0dbcdafe25d786e981ae2d39f0726
JSK0406 <12180543@inha.edu>
2024-08-20 22:34:37 +0900

    feat: modify useDrawingRankQuery (#233)

--- strawberry/src/data/queries/drawing/useDrawingRankQuery.tsx
+++ strawberry/src/data/queries/drawing/useDrawingRankQuery.tsx
@@ -3,16 +3,21 @@ import { useQuery } from "react-query";
 import network from "../../config/network";
 import { DrawingRank } from "../../entities/DrawingRank";
 
-export function useDrawingRankQuery() {
+interface UseDrawingRankQueryProps {
+  subEventId: number | undefined;
+}
+
+export function useDrawingRankQuery({ subEventId }: UseDrawingRankQueryProps) {
   const getDrawingRank = async () => {
     return await network.get<DrawingRank[]>("lottery/drawing/rank", {
-      queryParams: { subEventId: "4" },
+      queryParams: { subEventId: subEventId },
     });
   };
 
   const query = useQuery<DrawingRank[], Error>({
     queryKey: ["drawingRank"],
     queryFn: getDrawingRank,
+    enabled: false,
   });
 
   return query;
