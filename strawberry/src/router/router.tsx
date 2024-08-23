be910048d64a164413c9a876c0f2690466e9359e
akrudal <1mmgm.dev@gmail.com>
2024-08-23 13:36:50 +0900

    feat: add lazy loading(#293)

--- strawberry/src/router/router.tsx
+++ strawberry/src/router/router.tsx
@@ -1,3 +1,4 @@
+import React, { Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 import PublicRoute from "./PublicRoute";
@@ -6,19 +7,36 @@ import ProtectedRoute from "./ProtectedRoute";
 import DefaultLayout from "../layout/DefaultLayout";
 import HeaderLayout from "../layout/HeaderLayout";
 
-import QuizPlayWrapper from "../pages/quizPlay/QuizPlayWrapper";
-import ExpectationPageWrapper from "../pages/expectation/ExpectationPageWrapper";
-import QuizLandingWrapper from "../pages/quizLanding/QuizLandingWrapper";
-import DrawingLandingWrapper from "../pages/drawingLanding/DrawingLandingWrapper";
-import DrawingPlayWrapper from "../pages/drawingPlay/DrawingPlayWrapper";
-import SharedRedirectedPage from "../pages/shared/SharedRedirectedPage";
+// Dynamically import pages
+const QuizPlayWrapper = React.lazy(
+  () => import("../pages/quizPlay/QuizPlayWrapper"),
+);
+const ExpectationPageWrapper = React.lazy(
+  () => import("../pages/expectation/ExpectationPageWrapper"),
+);
+const QuizLandingWrapper = React.lazy(
+  () => import("../pages/quizLanding/QuizLandingWrapper"),
+);
+const DrawingLandingWrapper = React.lazy(
+  () => import("../pages/drawingLanding/DrawingLandingWrapper"),
+);
+const DrawingPlayWrapper = React.lazy(
+  () => import("../pages/drawingPlay/DrawingPlayWrapper"),
+);
+const SharedRedirectedPage = React.lazy(
+  () => import("../pages/shared/SharedRedirectedPage"),
+);
+const LandingPage = React.lazy(() => import("../pages/landing/LandingPage"));
+const LoginPage = React.lazy(() => import("../pages/login/LoginPage"));
+const LoginRedirectedPage = React.lazy(
+  () => import("../pages/login/LoginRedirectPage"),
+);
+const NewCarPageWrapper = React.lazy(
+  () => import("../pages/newCar/NewCarPageWrapper"),
+);
+const NotFound = React.lazy(() => import("../pages/error/NotFound"));
 
-import LandingPage from "../pages/landing/LandingPage";
-import LoginPage from "../pages/login/LoginPage";
-import LoginRedirectedPage from "../pages/login/LoginRedirectPage";
-import NewCarPageWrapper from "../pages/newCar/components/NewCarPageWrapper";
-
-import NotFound from "../pages/error/NotFound";
+const Loading = () => <div>Loading...</div>;
 
 const router = createBrowserRouter([
   {
@@ -28,69 +46,79 @@ const router = createBrowserRouter([
       {
         path: "login",
         element: (
-          <PublicRoute>
-            <LoginPage />
-          </PublicRoute>
+          <Suspense fallback={<Loading />}>
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          </Suspense>
         ),
       },
       {
         path: "",
         element: (
-          <>
+          <Suspense fallback={<Loading />}>
             <LandingPage />
-          </>
+          </Suspense>
         ),
       },
       {
         path: "introduce",
         element: (
-          <>
+          <Suspense fallback={<Loading />}>
             <NewCarPageWrapper />
-          </>
+          </Suspense>
         ),
       },
       {
         path: "expectation",
         element: (
-          <>
+          <Suspense fallback={<Loading />}>
             <ExpectationPageWrapper />
-          </>
+          </Suspense>
         ),
       },
       {
         path: "quiz",
-        element: <QuizLandingWrapper />,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <QuizLandingWrapper />
+          </Suspense>
+        ),
       },
       {
         path: "drawing",
-        element: <DrawingLandingWrapper />,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <DrawingLandingWrapper />
+          </Suspense>
+        ),
       },
     ],
   },
   {
     path: "/",
     element: (
-      <>
+      <Suspense fallback={<Loading />}>
         <HeaderLayout>
           <ProtectedRoute />
         </HeaderLayout>
-      </>
+      </Suspense>
     ),
     children: [
       {
         path: "quiz/play/:subEventId/:token",
         element: (
-          <>
+          <Suspense fallback={<Loading />}>
             <QuizPlayWrapper />
-          </>
+          </Suspense>
         ),
       },
       {
         path: "drawing/play/:subEventId",
         element: (
-          <>
+          <Suspense fallback={<Loading />}>
             <DrawingPlayWrapper />
-          </>
+          </Suspense>
         ),
       },
     ],
@@ -98,21 +126,29 @@ const router = createBrowserRouter([
   {
     path: "auth/:sns/callback",
     element: (
-      <PublicRoute>
-        <LoginRedirectedPage />
-      </PublicRoute>
+      <Suspense fallback={<Loading />}>
+        <PublicRoute>
+          <LoginRedirectedPage />
+        </PublicRoute>
+      </Suspense>
     ),
   },
   {
     path: "shared/:sharedCode",
-    element: <SharedRedirectedPage />,
+    element: (
+      <Suspense fallback={<Loading />}>
+        <SharedRedirectedPage />
+      </Suspense>
+    ),
   },
   {
     path: "*",
     element: (
-      <HeaderLayout>
-        <NotFound />
-      </HeaderLayout>
+      <Suspense fallback={<Loading />}>
+        <HeaderLayout>
+          <NotFound />
+        </HeaderLayout>
+      </Suspense>
     ),
   },
 ]);
