836b03138a496db2f59db0f917e9e693a59703db
akrudal <1mmgm.dev@gmail.com>
2024-08-24 18:05:26 +0900

    신차 소개 Driving Banner를 수정합니다

--- strawberry/src/pages/newCar/components/driving/DrivingBanner.tsx
+++ strawberry/src/pages/newCar/components/driving/DrivingBanner.tsx
@@ -1,8 +1,26 @@
-import { ImageEnum } from "../../../../core/design_system";
+import {
+  theme,
+  ImageEnum,
+  Label,
+  Wrapper,
+} from "../../../../core/design_system";
 
 function DrivingBanner() {
   return (
     <section>
+      <Wrapper
+        display="flex"
+        $flexdirection="column"
+        $alignitems="center"
+        $margin="0 0 40px 0"
+      >
+        <Label $token="Display1Medium" color={theme.Color.TextIcon.default}>
+          Driving
+        </Label>
+        <Label $token="Title2Regular" color={theme.Color.TextIcon.sub}>
+          일상과 아웃도어를 넘나들며 다양하게 공간을 활용할 수 있습니다.
+        </Label>
+      </Wrapper>
       <img
         loading="lazy"
         src={ImageEnum.IMAGES.NEWCAR.DRIVING_BANNER}
